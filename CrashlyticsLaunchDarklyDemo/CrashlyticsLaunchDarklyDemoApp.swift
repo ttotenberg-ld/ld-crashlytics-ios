425461186a38cc39d4be6d0ba02ef531b801d1a6
Tom Totenberg <ttotenberg@launchdarkly.com>
2025-03-19 11:16:50 -0500

    Initial Commit

---
+++ CrashlyticsLaunchDarklyDemo/CrashlyticsLaunchDarklyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrashlyticsLaunchDarklyDemoApp.swift
+//  CrashlyticsLaunchDarklyDemo
+//
+//  Created by Tom Totenberg on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct CrashlyticsLaunchDarklyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CrashlyticsLaunchDarklyDemoTests/CrashlyticsLaunchDarklyDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  CrashlyticsLaunchDarklyDemoTests.swift
+//  CrashlyticsLaunchDarklyDemoTests
+//
+//  Created by Tom Totenberg on 3/19/25.
+//
+
+import Testing
+@testable import CrashlyticsLaunchDarklyDemo
+
+struct CrashlyticsLaunchDarklyDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
